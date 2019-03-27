904e35a37b3995bf669add52471b54f7415770bc
Robert A. Petit III <robbie.petit@gmail.com>
2019-03-27 13:56:53 -0400

    output the per-base coverage against reference genomes

--- templates/call_variants.sh
+++ templates/call_variants.sh
@@ -4,10 +4,12 @@ set -u
 
 snippy !{fastq} --ref !{reference} --prefix !{sample} \
     --outdir !{reference_name} --cpus !{task.cpus}
-
 vcf-annotator !{reference_name}/!{sample}.vcf !{reference} > !{reference_name}/!{sample}-final.vcf
 
+# Clean Up
 rm -rf !{reference_name}/reference !{reference_name}/ref.fa* !{reference_name}/!{sample}.vcf.gz*
-
 find !{reference_name}/ -type f -not -name "*.bam*" -and -not -name "*.log*" -and -not -name "*.txt*" | \
     xargs -I {} pigz -n --best -p !{task.cpus} {}
+
+genomeCoverageBed -ibam !{reference_name}/!{sample}.bam -d | \
+    pigz -n --best -p !{task.cpus} - > !{reference_name}/!{sample}-coverages.gz
