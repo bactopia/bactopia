acc264919ac4833f32a971fff25f3f4bd8e51416
Robert A Petit III <robbie.petit@gmail.com>
2020-08-13 16:47:53 -0400

    update container build script

--- bin/build-containers.sh
+++ bin/build-containers.sh
@@ -2,7 +2,7 @@
 # build-containers
 #
 # Automate the building of Bactopia related containers
-VERSION=1.4.3
+VERSION=1.4.1
 CONTAINER_VERSION="${VERSION%.*}.x"
 
 function singularity_build {
@@ -13,7 +13,7 @@ function singularity_build {
     latest=${5:-0}
 
     echo "Working on ${recipe}"
-    singularity build -F --fakeroot ${image} ${recipe}
+    singularity build -F --remote ${image} ${recipe}
     singularity sign ${image}
     singularity push ${image} library://rpetit3/bactopia/${name}:${version}
 
@@ -55,35 +55,41 @@ if [ -z  ${BACTOPIA_DIR} ]; then
     echo "Must give the path to Bactopia repository"
     exit 1
 fi
+MAJOR_VERSION=${2:-"0"}
 
 mkdir -p ${OUTPUT_DIR}
 
-# Build Singularity
-singularity_build Singularity bactopia ${OUTPUT_DIR}/bactopia-${CONTAINER_VERSION}.simg ${CONTAINER_VERSION} 1
-for recipe in $(ls "${BACTOPIA_DIR}/containers/singularity" | grep ".Singularity"); do
-    recipe_path="${BACTOPIA_DIR}/containers/singularity/${recipe}"
-    recipe_name=$(echo ${recipe} | sed 's/.Singularity//')
-    recipe_image="${OUTPUT_DIR}/${recipe_name}-${CONTAINER_VERSION}.simg"
-    singularity_build ${recipe_path} ${recipe_name} ${recipe_image} ${CONTAINER_VERSION}
-done
+# Build Bactopia containers
+singularity_build Singularity bactopia ${OUTPUT_DIR}/bactopia-${VERSION}.simg ${VERSION} 1
+docker_build Dockerfile bactopia/bactopia:${VERSION} bactopia/bactopia:latest
 
-# Build Docker
-docker_build Dockerfile bactopia/bactopia:${CONTAINER_VERSION} bactopia/bactopia:latest
-for recipe in $(ls "${BACTOPIA_DIR}/containers/docker" | grep ".Dockerfile"); do
-    recipe_path="${BACTOPIA_DIR}/containers/docker/${recipe}"
-    recipe_name=$(echo ${recipe} | sed 's/.Dockerfile//')
-    recipe_image="bactopia/${recipe_name}:${CONTAINER_VERSION}"
-    docker_build ${recipe_path} ${recipe_image}
-done
+if [ "${MAJOR_VERSION}" == "1" ]; then
+    # Build Singularity
+    for recipe in $(ls "${BACTOPIA_DIR}/containers/singularity" | grep ".Singularity"); do
+        recipe_path="${BACTOPIA_DIR}/containers/singularity/${recipe}"
+        recipe_name=$(echo ${recipe} | sed 's/.Singularity//')
+        recipe_image="${OUTPUT_DIR}/${recipe_name}-${CONTAINER_VERSION}.simg"
+        singularity_build ${recipe_path} ${recipe_name} ${recipe_image} ${CONTAINER_VERSION}
+    done
 
-# Build Bactopia Tools containers
-for tool in $(ls "${BACTOPIA_DIR}/tools"); do
-    recipe_path="${BACTOPIA_DIR}/tools/${tool}"
-    docker_file="${recipe_path}/Dockerfile"
-    docker_image="bactopia/tools-${tool}:${CONTAINER_VERSION}"
-    docker_build ${docker_file} ${docker_image}
+    # Build Docker
+    docker_build Dockerfile bactopia/bactopia:${CONTAINER_VERSION} bactopia/bactopia:latest
+    for recipe in $(ls "${BACTOPIA_DIR}/containers/docker" | grep ".Dockerfile"); do
+        recipe_path="${BACTOPIA_DIR}/containers/docker/${recipe}"
+        recipe_name=$(echo ${recipe} | sed 's/.Dockerfile//')
+        recipe_image="bactopia/${recipe_name}:${CONTAINER_VERSION}"
+        docker_build ${recipe_path} ${recipe_image}
+    done
 
-    singularity_file="${recipe_path}/Singularity"
-    singularity_image="${OUTPUT_DIR}/tools-${tool}-${CONTAINER_VERSION}.simg"
-    singularity_build ${singularity_file} "tools-${tool}" ${singularity_image} ${CONTAINER_VERSION}
-done
+    # Build Bactopia Tools containers
+    for tool in $(ls "${BACTOPIA_DIR}/tools"); do
+        recipe_path="${BACTOPIA_DIR}/tools/${tool}"
+        docker_file="${recipe_path}/Dockerfile"
+        docker_image="bactopia/tools-${tool}:${CONTAINER_VERSION}"
+        docker_build ${docker_file} ${docker_image}
+
+        singularity_file="${recipe_path}/Singularity"
+        singularity_image="${OUTPUT_DIR}/tools-${tool}-${CONTAINER_VERSION}.simg"
+        singularity_build ${singularity_file} "tools-${tool}" ${singularity_image} ${CONTAINER_VERSION}
+    done
+fi
