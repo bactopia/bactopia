086920f110a5906372a18237b851f4e1d4eef603
Robert A Petit III <robbie.petit@gmail.com>
2020-10-14 14:26:49 -0400

    added fallback in case NCBI eutils is down

--- bin/select-references.py
+++ bin/select-references.py
@@ -4,13 +4,14 @@
 PROGRAM = "select-references"
 VERSION = "1.4.11"
 
-def check_assembly_version(accession):
+
+def use_eutils(accession):
     from Bio import Entrez
     import time
     import json
     Entrez.email = "robert.petit@emory.edu"
     Entrez.tool = "BactopiaSelectReferences"
-
+    accession = accession.split('.')[0]
     handle = Entrez.esearch(db="assembly", term=accession, retmax="500")
     record = Entrez.read(handle, validate=False)
     time.sleep(1) # Be kind to NCBI
@@ -34,6 +35,58 @@ def check_assembly_version(accession):
         return [','.join(list(excluded)), True]
     else:
         return [sorted(records, reverse=True)[0], False]
+
+
+def use_http(accession):
+    """
+    Use urllib to get a link.
+    Example GCF_001548295: https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/001/548/295/ 
+
+    Need to extract "<a href="GCF_001548295.1_ASM154829v1/">GCF_001548295.1_ASM154829v1/</a>"
+    """
+    import re
+    import requests
+    accession, version = accession.split('.')
+    db, digits = accession.split("_")
+    digits_split = '/'.join(re.findall('.{1,3}', digits))
+    url = f'https://ftp.ncbi.nlm.nih.gov/genomes/all/{db}/{digits_split}'
+    
+    r = requests.get(url)
+    current_accession = []
+    if r.status_code == 200: 
+        # Success
+        links = re.findall("href=[\"\'](.*?)[\"\']", r.text)
+        for link in links:
+            if link.startswith(accession):
+                t_db, t_version, t_extra = link.split("_", 2)
+                current_accession.append(f"{t_db}_{t_version}")
+
+        if len(current_accession) == 1:
+            return [current_accession[0], False, None, None]
+        else:
+            if not len(current_accession):
+                return [current_accession, False, True, "Unable to parse and accession"]
+            else:
+                return [sorted(current_accession, reverse=True)[0], False, None, None]
+        
+    else:
+        return [accession, True, False, f"Accession does not exist at {url}, status code {r.status_code}"]
+
+
+def check_assembly_version(accession):
+    try:
+        return use_eutils(accession)
+    except Exception as e:
+        if e.msg == "Bad Gateway":
+            print("NCBI servers are down, trying fallback.", file=sys.stderr)
+            current_accession, excluded, has_error, reason = use_http(accession)
+            if has_error:
+                print(f"Fallback failed. Reason: {reason}", file=sys.stderr)
+                sys.exit(42)
+            else:
+                return [current_accession, excluded]
+        else:
+            sys.exit(1)
 
 
 if __name__ == '__main__':
@@ -88,7 +141,7 @@ if __name__ == '__main__':
 
         for reference in references:
             if reference:
-                print(reference)
+                print(use_http(reference))
                 current_accession, excluded = check_assembly_version(reference)
                 if excluded:
                     print(
