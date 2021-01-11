0a90c54fff59513528a016d571b254dfc6a54deb
Robert A. Petit III <robbie.petit@gmail.com>
2021-01-11 11:44:38 -0500

    Update and rename environment.yml to setup-action-env.sh

---
+++ setup-action-env.sh
@@ -0,0 +1,20 @@
+conda install -y -c conda-forge -c bioconda \
+  ariba \
+  beautifulsoup4 \
+  biopython \
+  blast \
+  bowtie2 <2.4.0  \
+  cd-hit \
+  conda \
+  coreutils \
+  executor \
+  lxml \
+  mash \
+  ncbi-genome-download \
+  nextflow \
+  pysam >=0.15.3 \
+  python >3.6 \
+  requests  \
+  sed \
+  unzip \
+  wget
