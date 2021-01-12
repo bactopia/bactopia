2b0b2ee40ca6f486f579b739e217af9cfe8d1245
Robert A Petit III <robbie.petit@gmail.com>
2021-01-12 12:36:13 -0500

    more fixes docker builder script

--- bin/update-docker.sh
+++ bin/update-docker.sh
@@ -2,7 +2,6 @@
 # update-docker
 #
 # Automate the building of Bactopia related Docker containers
-REPOSITORY="quay.io"
 VERSION=1.5.5
 CONTAINER_VERSION="${VERSION%.*}.x"
 BACTOPIA_DIR=${1:-"./"}
@@ -23,13 +22,13 @@ function docker_build {
 }
 
 # Build Bactopia Container
-docker_build Dockerfile ${REPOSITORY}/bactopia/bactopia:${VERSION} ${REPOSITORY}/bactopia/bactopia:latest
+docker_build Dockerfile bactopia/bactopia:${VERSION} bactopia/bactopia:latest
 
 # Build Process Containers
 for recipe in $(ls "${BACTOPIA_DIR}/containers/docker" | grep ".Dockerfile"); do
     recipe_path="${BACTOPIA_DIR}/containers/docker/${recipe}"
     recipe_name=$(echo ${recipe} | sed 's/.Dockerfile//')
-    recipe_image="${REPOSITORY}/bactopia/${recipe_name}:${CONTAINER_VERSION}"
+    recipe_image="bactopia/${recipe_name}:${CONTAINER_VERSION}"
     docker_build ${recipe_path} ${recipe_image}
 done
 
@@ -38,7 +37,7 @@ for tool in $(ls "${BACTOPIA_DIR}/tools"); do
     recipe_path="${BACTOPIA_DIR}/tools/${tool}"
     if [ -f "${BACTOPIA_DIR}/tools/${tool}/environment-linux.yml" ]; then
         docker_file="${recipe_path}/Dockerfile"
-        docker_image="${REPOSITORY}/bactopia/tools-${tool}:${CONTAINER_VERSION}"
+        docker_image="bactopia/tools-${tool}:${CONTAINER_VERSION}"
         docker_build ${docker_file} ${docker_image}
     fi
 done
