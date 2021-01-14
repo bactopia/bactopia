c2ffc07c2ca813a7a9f238deb9031212711ad9ad
Robert A Petit III <robbie.petit@gmail.com>
2021-01-14 01:30:14 -0500

    fixed docker push path

--- bin/update-docker.sh
+++ bin/update-docker.sh
@@ -48,13 +48,13 @@ function docker_build {
 }
 
 # Build Bactopia Container
-docker_build Dockerfile bactopia:${VERSION} bactopia:latest
+docker_build Dockerfile bactopia/bactopia:${VERSION} bactopia/bactopia:latest
 
 # Build Process Containers
 for recipe in $(ls "${BACTOPIA_DIR}/containers/docker" | grep ".Dockerfile"); do
     recipe_path="${BACTOPIA_DIR}/containers/docker/${recipe}"
     recipe_name=$(echo ${recipe} | sed 's/.Dockerfile//')
-    recipe_image="${recipe_name}:${CONTAINER_VERSION}"
+    recipe_image="bactopia/${recipe_name}:${CONTAINER_VERSION}"
     docker_build ${recipe_path} ${recipe_image}
 done
 
@@ -63,7 +63,7 @@ for tool in $(ls "${BACTOPIA_DIR}/tools"); do
     recipe_path="${BACTOPIA_DIR}/tools/${tool}"
     if [ -f "${BACTOPIA_DIR}/tools/${tool}/environment-linux.yml" ]; then
         docker_file="${recipe_path}/Dockerfile"
-        docker_image="tools-${tool}:${CONTAINER_VERSION}"
+        docker_image="bactopia/tools-${tool}:${CONTAINER_VERSION}"
         docker_build ${docker_file} ${docker_image}
     fi
 done
