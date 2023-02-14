8f8b59c2cae2bb430eece5618022f4d3874a2fd4
Robert A. Petit III <robbie.petit@gmail.com>
2023-02-14 20:38:37 +0000

    try that conda recipe again

---
+++ data/conda/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+BACTOPIA_VERSION="${PKG_VERSION%.*}.x"
+BACTOPIA="${PREFIX}/share/${PKG_NAME}-${BACTOPIA_VERSION}"
+mkdir -p ${PREFIX}/bin ${BACTOPIA}
+
+chmod 775 bin/*.py
+cp bin/*.py ${PREFIX}/bin
+
+chmod 775 bin/bactopia/*
+cp bin/bactopia/* ${PREFIX}/bin
+
+# Install bactopia-py
+$PYTHON -m pip install . --no-deps --ignore-installed -vv
+
+# Move bactopia nextflow
+mv bin/ conf/ data/ lib/ modules/ subworkflows/ tests/ workflows/ main.nf nextflow.config ${BACTOPIA}
